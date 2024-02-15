83a8135573e45c43d48d332228fa77f2290f89ce
w4n9hu1 <wanghui89757@gmail.com>
2024-02-15 20:28:25 +0800

    install react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
-import { Button } from "@mui/material"
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom"
+
+function Task() {
+  return (
+    <>
+      <h1 className="text-xl text-blue-600">Task</h1>
+    </>
+  )
+}
+
+function User() {
+  return (
+    <>
+      <h1 className="text-xl text-blue-600">User</h1>
+    </>
+  )
+}
+
+function Layout() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/user">User</Link>
+          </li>
+          <li>
+            <Link to="/task">Task</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+}
 
 function App() {
   return (
     <>
-      <h1 className="text-xl text-blue-600">App</h1>
-      <Button variant="contained">Hello world</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<User />} />
+            <Route path="user" element={<User />} />
+            <Route path="task" element={<Task />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }

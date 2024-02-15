665486866c47dd8d3a2b04fb6c353ad8dc344bbe
w4n9hu1 <wanghui89757@gmail.com>
2024-02-15 20:15:39 +0800

    install material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material"
+
 function App() {
   return (
     <>
       <h1 className="text-xl text-blue-600">App</h1>
+      <Button variant="contained">Hello world</Button>
     </>
   )
 }

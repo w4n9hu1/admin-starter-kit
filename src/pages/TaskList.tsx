4c11aadfefc42682c6526d4605921db4cc165ed8
w4n9hu1 <wanghui89757@gmail.com>
2024-02-24 16:31:48 +0800

    grid

--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -1,63 +1,36 @@
-import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers"
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { Box, Button, FormControl, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material";
-import { useState } from "react";
+import { Grid } from "@mui/material";
 
 export default function TaskList() {
-    const [taskStatus, setTaskStatus] = useState('open');
-
     return (
-        <div className="w-full space-y-3">
+        <div className="w-full flex flex-col">
+            <Grid container spacing={2}>
+                <Grid item xs={9} container spacing={2}>
+                    <Grid item xs={6} md={4} lg={3}>
+                        <div className="bg-red-300">1-1</div>
+                    </Grid>
+                    <Grid item xs={6} md={4} lg={3}>
+                        <div className="bg-red-300">1-2</div>
+                    </Grid>
+                    <Grid item xs={6} md={4} lg={3}>
+                        <div className="bg-red-300">1-2</div>
+                    </Grid>
+                    <Grid item xs={6} md={4} lg={3}>
+                        <div className="bg-red-300">1-1</div>
+                    </Grid>
+                    <Grid item xs={6} md={4} lg={3}>
+                        <div className="bg-red-300">1-1</div>
+                    </Grid>
+                </Grid>
+                <Grid item xs={3} display="flex" justifyContent="center" alignItems="center">
+                    2
+                </Grid>
+            </Grid>
             <div>
-                <form action="">
-                    <div className="flex items-center">
-                        <div>
-
-                            <Box
-                                component="form"
-                                sx={{
-                                    '& > :not(style)': { my: 1, mx: 3, width: '30ch' },
-                                }}
-                                autoComplete="off"
-                            >
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DateTimePicker label="Task created From" />
-                                </LocalizationProvider>
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DateTimePicker label="Task created To" />
-                                </LocalizationProvider>
-                                <TextField variant="outlined" label="Task title" />
-                                <FormControl >
-                                    <InputLabel id="demo-simple-select-label">Status</InputLabel>
-                                    <Select
-                                        labelId="demo-simple-select-label"
-                                        value={taskStatus}
-                                        label="Status"
-                                        onChange={() => setTaskStatus('')}
-                                    >
-                                        <MenuItem value={10}>Open</MenuItem>
-                                        <MenuItem value={20}>In-Progress</MenuItem>
-                                        <MenuItem value={30}>Done</MenuItem>
-                                    </Select>
-                                </FormControl>
-                                <TextField variant="outlined" label="Task decription" />
-                            </Box>
-                        </div>
-                        <div className="p-6">
-                            <Button type="submit" variant="contained">Search</Button>
-                        </div>
-                    </div>
-                </form >
+                actions
             </div>
-            <div>
-                <Stack spacing={2} direction="row">
-                    <Button variant="contained">Create Task</Button>
-                    <Button variant="outlined">Export</Button>
-                </Stack>
-            </div>
-            <div>
+            <div className="bg-red-300 grow">
                 table
             </div>
-        </div >
+        </div>
     )
 }

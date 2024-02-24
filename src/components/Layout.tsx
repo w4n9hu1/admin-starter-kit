c21ec25f28794ffc58d2772d400299abf5e0811f
w4n9hu1 <wanghui89757@gmail.com>
2024-02-24 16:08:20 +0800

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,11 @@
-import { ThemeProvider, Tooltip } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { Navigate, Outlet } from "react-router-dom";
 import theme from "./theme";
 import SideBar from "./SideBar";
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { useEffect, useState } from "react";
 import { getUserAuthConfig } from "../api/auth";
 
 export function Layout() {
-    const [openSidebar, setOpenSidebar] = useState(true);
     const [userAuthConfig, setUserAuthConfig] = useState<UserAuthConfig>({
         sidebarItems: [],
         userInfo: { username: '', email: '' },
@@ -33,16 +30,9 @@ export function Layout() {
         <>
             <ThemeProvider theme={theme}>
                 <div className="flex h-full">
-                    {openSidebar && <SideBar companyName={userAuthConfig.companyName}
+                    <SideBar companyName={userAuthConfig.companyName}
                         userInfo={userAuthConfig.userInfo}
-                        menuList={userAuthConfig.sidebarItems} />}
-                    <div className="flex items-center bg-gray-100">
-                        <div className="cursor-pointer text-zinc-600 hover:text-zinc-900" onClick={() => setOpenSidebar(!openSidebar)}>
-                            {openSidebar ? <Tooltip title="Close sidebar" arrow><KeyboardArrowLeftIcon /></Tooltip> :
-                                <Tooltip title="Open sidebar" arrow><KeyboardArrowRightIcon /></Tooltip>
-                            }
-                        </div>
-                    </div>
+                        menuList={userAuthConfig.sidebarItems} />
                     <main className="bg-gray-100 w-full flex flex-col divide-y divide-gray-300">
                         <div className="p-6">
                             <h1 className="text-lg font-bold">Subtitle</h1>
